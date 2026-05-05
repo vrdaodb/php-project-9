d5e315978f61f0206edf5fce4f69e5bd23ce8b33
study <veritasdevar@gmail.com>
2026-05-05 16:48:32 +0000

    Bootstrap

--- public/index.php
+++ public/index.php
@@ -1,15 +1,25 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+$container->set('renderer', function () {
+    return new PhpRenderer(__DIR__ . '/../templates', ['flash' => []]);
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Hello, World!');
-    return $response;
+    $content = $this->get('renderer')->fetch('index.phtml');
+    return $this->get('renderer')->render($response, 'layout.phtml', [
+        'content' => $content
+    ]);
 });
 
 $app->run();
